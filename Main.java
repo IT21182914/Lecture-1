d06300adf36a83af9803a3bddc66510ec49a8500
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2022-07-25 12:44:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,73 @@
 import java.util.Scanner;
-
-public class Main {
-
-  public static void main(String[] args) {
-
-    Scanner inputs = new Scanner(System.in);
-
-    System.out.println("Enter First integer:");
-    int num1 = inputs.nextInt();
-
-    System.out.println("Enter Second integer:");
-    int num2 = inputs.nextInt();
-
-    System.out.println("Enter Third integer:");
-    int num3 = inputs.nextInt();
-
-    int largest;
-
-    if (num1 > num2 && num1 > num3) {
-      largest = num1;
-    }
-
-    else if (num2 > num1 && num2 > num3) {
-      largest = num2;
-    } else {
-      largest = num3;
-    }
-
-    System.out.println("Max is: " + largest);
-
-  }
+public class Max {
+	
+	public static void main (String [] args) {
+		
+		
+		Scanner inputs = new Scanner (System.in);
+		
+		
+		System.out.println("Enter First integer:");
+		int num1 = inputs.nextInt();
+		
+		
+		System.out.println("Enter Second integer:");
+		int num2 = inputs.nextInt();
+		
+		
+		
+		System.out.println("Enter Third integer:");
+		int num3 = inputs.nextInt();
+		
+		int largest = 0;
+		int smallest = 0;
+		
+		if(num1>num2 && num1>num3)
+		{
+			largest = num1;
+			
+			if(num2<num3)
+			{
+				
+				smallest = num2;
+				
+			}
+			else {
+				smallest= num3;
+				
+			}
+		}
+		
+		else if(num2>num1 && num2>num3)
+		{
+			largest=num2;
+			
+			if(num1<num3)
+			{
+				smallest = num1;
+			}
+			else {
+				smallest = num3;
+			}
+		}
+		else
+		{
+			largest = num3;
+			
+			if(num1<num3)
+			{
+				smallest = num1;
+			}
+			else {
+				smallest = num3;
+			}
+			
+		}
+		
+		System.out.println("Max is: "+ largest);
+		System.out.println("Min is: "+ smallest);
+		
+		 
+	}
 
 }
